9fd175861bddeea6f8586cbe57b69c2b5cd4996a
Cristian Damian <damiancc28@gmail.com>
2018-04-02 17:26:17 +0300

    Inital commit.

---
+++ cs_fista.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from skimage.restoration import denoise_tv_chambolle
+
+    
+class CS_solver_FISTA:
+    """
+    Class for making Compressive Sensing reconstrunctions using the  
+    Fast Shrinkage Thresholding Algorithm (FISTA).   
+    
+    """
+    
+    def __init__(self, 
+                 forward,
+                 proximal=None,
+                 backward=None, 
+                 weight = 1,
+                 tol=2.0e-4,
+                 maxiter=200):
+        """
+        Initalize parameters for the reconstruction algorithm:
+            
+        Parameters
+        ----------
+        forward : 2d ndarray, callable (ndarray -> ndarray)
+            The measurement operator. A function that takes a latent image 
+            and returns its noseless measurements.
+            
+        backward : callable (ndarray -> ndarray)
+            The adjoint of the measurement operator. Necesary if forward is 
+            given a callable.
+            
+        prox_op: string, callable (ndarray y, float lmb -> ndarray x)
+            Proximal operator used for regularisation. The proximal operator
+            solves::
+                
+                argmin x of sum((x-y)**2) + lmb*f(x)
+            
+            - If 'l1' of None (default) then L1-norm regularisation is used.
+            
+            - If 'tv' then Total Variation regularisation is used using 
+              Chambolle's algorithm.
+            
+        tol : float, optional
+            Difference between succesive iterates at witch to stop the algorithm.
+            The algorithm stops when: 
+            
+                max(abs(X1-X)) < eps
+                
+        maxiter : int, optional
+            Maximum number of iterations of the algorithm.
+            
+        References
+        ----------
+            Beck, A. & Teboulle, M. 
+            A Fast Iterative Shrinkage-Thresholding Algorithm for 
+            Linear Inverse Problems. 
+            SIAM Journal on Imaging Sciences, 2009, 2, 183-202
+                    
+        """
+        
+        if forward is None:
+            raise TypeError("Forward can not be `None`")
+            
+        elif np.ndim(forward) == 2 and all(np.isfinite(forward).flat):
+            forward = np.asarray(forward)
+            self.forward = lambda x: forward @ x
+            self.backward = lambda x: forward.T @ x
+            
+        elif callable(forward):
+            if not callable(backward):
+                raise ValueError(
+                    "if `forward` is a function then its adjoint must be given as `backward`.")
+            self.forward = forward
+            self.backward = backward
+            
+        else:
+            raise ValueError(
+                "`forward` must be a matrix with real numbers or callable.")   
+            
+        if proximal is None or proximal == 'l1':
+            self.proximal = lambda x, w: x - np.clip(x,-w,w)
+        
+        elif proximal == 'tv':
+            self.proximal = denoise_tv_chambolle
+            
+        elif callable(proximal):
+            self.proximal = proximal
+            
+        else:
+            raise ValueError(
+                "`proximal` must be a callable or a keword string.")
+            
+        self.weight = weight
+        self.tol = tol
+        self.maxiter = maxiter
+        
+        
+    def reconstruct(self, data, weight=None, guess=None):
+        """
+        Reconstructs the image from the given measurements.
+        
+        Parameters
+        ----------
+        data : ndarray
+            The measurements from witch to reconstruct the image.
+        
+        weight : float, optional
+            The regularisation weight. If none then 
+        
+        guess : numpy.ndarray, optional
+            The initial guess of the reconstruction.
+            
+        Returns
+        -------
+            X : ndarray
+            The reconstructed image.
+        
+        """
+        
+        phi = self.forward
+        phit = self.backward
+        D = np.asarray(data)
+        
+        if weight is None:
+            weight = self.weight
+        
+        if guess is None: 
+            X = phit(D)
+        else:
+            X = np.asarray(guess)
+        
+        laststep = 1    
+        m = 0
+        t = 1
+        Y = X
+        
+        while laststep > self.tol and m < self.maxiter:
+            m +=1
+            
+            # Gradient descent
+            r = -phit(phi(Y) - D)
+            mu = np.sum(r**2)/np.sum(r*phit(phi(r)))
+            mu = mu if np.isfinite(mu) else 1
+            V = Y + mu * r
+            
+            X1 = self.proximal(V, 2*mu*weight)
+            
+            # Nesterov acceleration 
+            t1 = (1 + (1+ t**2)**.5)/2
+            Y = X1 + (t-1)/t1 * (X1 - X)
+            
+            laststep = np.max(abs(X1-X))
+            X = X1
+            
+        return X
+
+    
+    def project(self, X):
+        """
+        Generate noiseless measrements from an image X.
+        """
+        return self.froward(X)
+    
+    def backproject(self, D):
+        """
+        Generate a backprojetion of the image using the measurements D. 
+        """
+        return self.backward(D)
+       
+    
---
+++ cs_fista_demo.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 16 16:56:39 2018
+
+@author: DamianCristian
+
+cs_fista_demo.py
+
+Usage demonstration of cs_fista.py
+
+"""
+
+import numpy as np
+from skimage import data
+from scipy.linalg import hadamard
+from scipy.ndimage import convolve1d
+from matplotlib import pyplot as plt
+plt.rcParams['image.cmap']='gray'
+from time import clock    
+
+from cs_fista import CS_solver_FISTA 
+
+def total_variation(X):
+    """
+    Computes the Total Variation of an array.
+    """
+    h=[-1,1]
+    g = np.zeros_like(X)
+    for i in range(0,X.ndim):
+        g += convolve1d(X,h,axis=i)**2
+    return np.sum(g**.5)
+    
+n = 128**2
+m = int(0.2 * n);
+
+print("Number of samples =",n)
+print("Number of measurements =",m)
+
+# Original Image      
+ishape = (n**.5,)*2
+I = data.binary_blobs(ishape[0])
+I = np.asarray(I, dtype=float)
+print('Image TV=', total_variation(I))
+
+# Measurement matrix
+H = hadamard(n) 
+np.random.shuffle(H)
+H = H.T[:m, :]/np.sqrt(n)
+
+# Defining measurement operator and adjoint 
+p = lambda x: H @ x.flat[:]
+pt = lambda x: np.reshape(H.T @ x, ishape)
+
+# Measurements
+D = p(I)
+
+# Least Squares estimate
+t1 = clock()
+e0 = pt(D)
+E0 = np.reshape(e0, ishape)
+runtime = clock()-t1
+
+print('LS Runtime=', runtime)
+print('LS PSNR=',10*np.log10(1/np.var(E0-I)))  
+
+solver = CS_solver_FISTA(p, 'tv', pt, 1.0e-2)
+t1 = clock()
+E = solver.reconstruct(D)
+runtime = clock()-t1
+
+print('TV Runtime=', runtime)
+print('TV PSNR=', 10*np.log10(1/np.var(E-I)))  
+
+f, s = plt.subplots(1,3)
+s[0].imshow(E0)
+s[0].set_title("LS reconstrucion")
+s[1].imshow(E)
+s[1].set_title("TV reconstrucion")
+s[2].imshow(I)
+s[2].set_title("Original Image")
+plt.show()
